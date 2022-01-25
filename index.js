004c4493a77cbc251315926ed881202525792da6
Gladwin <gladwin.dos@hotmail.co.uk>
2022-01-25 15:30:33 +0000

    map updates

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-const locations = [
+const employeeData = [
   {
-    postcode: "GU1 3XN",
-    latlng: {
-      lat: 51.2304098,
-      lng: -0.5708517000000001,
-    },
-  },
-  {
-    postcode: "GU14 8SX",
-    latlng: {
-      lat: 51.30529240000001,
-      lng: -0.7712819,
-    },
-  },
-  {
-    postcode: "GU9 8WF",
-    latlng: {
-      lat: 51.2044205,
-      lng: -0.8154511999999999,
-    },
-  },
-  {
+    name: "Pete White",
     postcode: "GU15 2BY",
     latlng: {
       lat: 51.3345648,
@@ -28,41 +8,7 @@ const locations = [
     },
   },
   {
-    postcode: "GU1 2PZ",
-    latlng: {
-      lat: 51.2402494,
-      lng: -0.5418086,
-    },
-  },
-  {
-    postcode: "IP25 7DR",
-    latlng: {
-      lat: 52.63165069999999,
-      lng: 0.7869813000000001,
-    },
-  },
-  {
-    postcode: "GU8 5JH",
-    latlng: {
-      lat: 51.1715379,
-      lng: -0.6505613,
-    },
-  },
-  {
-    postcode: "GU8 5RA",
-    latlng: {
-      lat: 51.1579085,
-      lng: -0.6571220999999999,
-    },
-  },
-  {
-    postcode: "GU14 9PJ",
-    latlng: {
-      lat: 51.3029591,
-      lng: -0.7803241,
-    },
-  },
-  {
+    name: "Gavin Shinfield",
     postcode: "GU1 3SE",
     latlng: {
       lat: 51.235704,
@@ -70,6 +16,55 @@ const locations = [
     },
   },
   {
+    name: "Harry Ford",
+    postcode: "GU9 8WF",
+    latlng: {
+      lat: 51.2044205,
+      lng: -0.8154511999999999,
+    },
+  },
+  {
+    name: "Piers Palmer Witley",
+    postcode: "GU8 5RA",
+    latlng: {
+      lat: 51.1579085,
+      lng: -0.6571220999999999,
+    },
+  },
+  {
+    name: "Andy West",
+    postcode: "IP25 7DR",
+    latlng: {
+      lat: 52.63165069999999,
+      lng: 0.7869813000000001,
+    },
+  },
+  {
+    name: "Nick Linnell",
+    postcode: "GU8 5JH",
+    latlng: {
+      lat: 51.1715379,
+      lng: -0.6505613,
+    },
+  },
+  {
+    name: "Steven Wake",
+    postcode: "GU14 8SX",
+    latlng: {
+      lat: 51.30529240000001,
+      lng: -0.7712819,
+    },
+  },
+  {
+    name: "Olly Percival ",
+    postcode: "GU1 2PZ",
+    latlng: {
+      lat: 51.2402494,
+      lng: -0.5418086,
+    },
+  },
+  {
+    name: "Duncan Roberston",
     postcode: "GU7 1LU",
     latlng: {
       lat: 51.1897945,
@@ -77,6 +72,23 @@ const locations = [
     },
   },
   {
+    name: "David Quilter ",
+    postcode: "GU14 9PJ",
+    latlng: {
+      lat: 51.3029591,
+      lng: -0.7803241,
+    },
+  },
+  {
+    name: "Laurent Maguire",
+    postcode: "GU1 3XN",
+    latlng: {
+      lat: 51.2304098,
+      lng: -0.5708517000000001,
+    },
+  },
+  {
+    name: "Chris Cannacott",
     postcode: "KT23 3AQ",
     latlng: {
       lat: 51.2790904,
@@ -84,6 +96,7 @@ const locations = [
     },
   },
   {
+    name: "Amy Johnstone",
     postcode: "GU21 3JP",
     latlng: {
       lat: 51.3197705,
@@ -91,6 +104,7 @@ const locations = [
     },
   },
   {
+    name: "Helen Beardshall (nee McCarthy)",
     postcode: "RG25 2RB",
     latlng: {
       lat: 51.230375,
@@ -98,6 +112,7 @@ const locations = [
     },
   },
   {
+    name: "Steve Butler",
     postcode: "GU2 7RT",
     latlng: {
       lat: 51.2297397,
@@ -105,6 +120,7 @@ const locations = [
     },
   },
   {
+    name: "Tom Marshall",
     postcode: "RG41 2AW",
     latlng: {
       lat: 51.4055644,
@@ -112,6 +128,7 @@ const locations = [
     },
   },
   {
+    name: "Jess Mitchell",
     postcode: "GU1 4LN",
     latlng: {
       lat: 51.2390903,
@@ -119,6 +136,7 @@ const locations = [
     },
   },
   {
+    name: "Darby Manning",
     postcode: "GU12 4RD",
     latlng: {
       lat: 51.2457928,
@@ -126,6 +144,7 @@ const locations = [
     },
   },
   {
+    name: "David Cocks",
     postcode: "GU1 1DN",
     latlng: {
       lat: 51.2470432,
@@ -133,6 +152,7 @@ const locations = [
     },
   },
   {
+    name: "Zoe Turner",
     postcode: "GU21 4SD",
     latlng: {
       lat: 51.32341479999999,
@@ -140,6 +160,7 @@ const locations = [
     },
   },
   {
+    name: "Jamie Hunter",
     postcode: "GU11 1FL",
     latlng: {
       lat: 51.2537542,
@@ -147,6 +168,7 @@ const locations = [
     },
   },
   {
+    name: "Will Poole",
     postcode: "KT22 9BX",
     latlng: {
       lat: 51.2905929,
@@ -154,6 +176,7 @@ const locations = [
     },
   },
   {
+    name: "Richard Brett",
     postcode: "GU5 0EW",
     latlng: {
       lat: 51.1908865,
@@ -161,6 +184,7 @@ const locations = [
     },
   },
   {
+    name: "Damian Boni",
     postcode: "SY23 2HN",
     latlng: {
       lat: 52.4177639,
@@ -168,6 +192,7 @@ const locations = [
     },
   },
   {
+    name: "Chris Myers",
     postcode: "GU35 0FU",
     latlng: {
       lat: 51.1187961,
@@ -175,6 +200,7 @@ const locations = [
     },
   },
   {
+    name: "Michael Hogg",
     postcode: "KT22 7JL",
     latlng: {
       lat: 51.296141,
@@ -182,6 +208,7 @@ const locations = [
     },
   },
   {
+    name: "Josh Jiminez",
     postcode: "GU1 2SP",
     latlng: {
       lat: 51.2413246,
@@ -189,6 +216,7 @@ const locations = [
     },
   },
   {
+    name: "Becky Baxter",
     postcode: "GU17 9FL",
     latlng: {
       lat: 51.3048835,
@@ -196,6 +224,7 @@ const locations = [
     },
   },
   {
+    name: "Ian Robinson",
     postcode: "GU51 1BW",
     latlng: {
       lat: 51.2923534,
@@ -203,6 +232,7 @@ const locations = [
     },
   },
   {
+    name: "Simon Hardy",
     postcode: "RG24 8BT",
     latlng: {
       lat: 51.302658,
@@ -210,13 +240,15 @@ const locations = [
     },
   },
   {
-    postcode: "GU27SN",
+    name: "Andrew Peng",
+    postcode: "GU2 7SN",
     latlng: {
       lat: 51.23764250000001,
       lng: -0.6001093,
     },
   },
   {
+    name: "Andy Pike",
     postcode: "GU21 8AG",
     latlng: {
       lat: 51.3156774,
@@ -224,6 +256,7 @@ const locations = [
     },
   },
   {
+    name: "Mark Gangel",
     postcode: "GU4 7YW",
     latlng: {
       lat: 51.2564133,
@@ -231,6 +264,7 @@ const locations = [
     },
   },
   {
+    name: "Charlotte Bell",
     postcode: "PL13 2QR",
     latlng: {
       lat: 50.3315168,
@@ -238,6 +272,7 @@ const locations = [
     },
   },
   {
+    name: "Rebecca Brennan",
     postcode: "GU1 3SA",
     latlng: {
       lat: 51.23690939999999,
@@ -245,6 +280,7 @@ const locations = [
     },
   },
   {
+    name: "Scott Matthewman",
     postcode: "E14 0BN",
     latlng: {
       lat: 51.5086542,
@@ -252,6 +288,7 @@ const locations = [
     },
   },
   {
+    name: "Farhad Ajaz",
     postcode: "KT19 0SB",
     latlng: {
       lat: 51.360114,
@@ -259,6 +296,7 @@ const locations = [
     },
   },
   {
+    name: "Rob Wills",
     postcode: "BH31 7PT",
     latlng: {
       lat: 50.8824254,
@@ -266,6 +304,7 @@ const locations = [
     },
   },
   {
+    name: "Guillaume Lannes",
     postcode: "W14 0AL",
     latlng: {
       lat: 51.5009324,
@@ -273,6 +312,7 @@ const locations = [
     },
   },
   {
+    name: "Tharindra Perera",
     postcode: "KT15 1FQ",
     latlng: {
       lat: 51.359451,
@@ -280,6 +320,7 @@ const locations = [
     },
   },
   {
+    name: "Robin Marsden",
     postcode: "HA9 7SF",
     latlng: {
       lat: 51.5557477,
@@ -287,6 +328,7 @@ const locations = [
     },
   },
   {
+    name: "Henry Lo",
     postcode: "WD18 7BL",
     latlng: {
       lat: 51.65609060000001,
@@ -294,6 +336,7 @@ const locations = [
     },
   },
   {
+    name: "Claire Chase",
     postcode: "TW19 5EN",
     latlng: {
       lat: 51.4622156,
@@ -301,6 +344,7 @@ const locations = [
     },
   },
   {
+    name: "Henry Price",
     postcode: "GU5 0DY",
     latlng: {
       lat: 51.1978346,
@@ -308,6 +352,7 @@ const locations = [
     },
   },
   {
+    name: "Mike Dunkling",
     postcode: "TN23 3RU",
     latlng: {
       lat: 51.1547279,
@@ -315,6 +360,7 @@ const locations = [
     },
   },
   {
+    name: "Louise Green",
     postcode: "SW17 0PP",
     latlng: {
       lat: 51.43430129999999,
@@ -322,6 +368,7 @@ const locations = [
     },
   },
   {
+    name: "Madis Lond",
     postcode: "HP2 5BX",
     latlng: {
       lat: 51.7572602,
@@ -329,6 +376,7 @@ const locations = [
     },
   },
   {
+    name: "Melissa Mays",
     postcode: "GU1 2BZ",
     latlng: {
       lat: 51.2369247,
@@ -336,6 +384,7 @@ const locations = [
     },
   },
   {
+    name: "Gladwin Dosunmu",
     postcode: "CR4 1QB",
     latlng: {
       lat: 51.395908,
@@ -343,6 +392,7 @@ const locations = [
     },
   },
   {
+    name: "Saranne Richter",
     postcode: "W1H 5AS",
     latlng: {
       lat: 51.5154412,
@@ -350,6 +400,7 @@ const locations = [
     },
   },
   {
+    name: "Ben Horsley-Summer",
     postcode: "GU12 6BS",
     latlng: {
       lat: 51.2521403,
@@ -358,12 +409,14 @@ const locations = [
   },
 ];
 
+const twentyMilesInMetres = 32186.9;
+const thirtyMilesInMetres = 48280.3;
+
 let map;
 let geocoder;
 
 function initMap() {
   geocoder = new google.maps.Geocoder();
-
   const officeLocation = { lat: 51.23535, lng: -0.57407 };
 
   map = new google.maps.Map(document.getElementById("map"), {
@@ -372,30 +425,121 @@ function initMap() {
   });
 
   // plot office location
-  new google.maps.Marker({
+  const officeMarker = new google.maps.Marker({
     position: officeLocation,
     map: map,
   });
+  addInfoWindowToMarker(map, officeMarker, "Office");
 
   // draw radius around office
   new google.maps.Circle({
     map: map,
     center: officeLocation,
-    radius: 32186.9, // 20 miles in metres
-    fillColor: "#fff",
+    radius: twentyMilesInMetres,
+    fillColor: "red",
+    strokeColor: "red",
   });
+  addLabelToRadius(map, officeLocation, twentyMilesInMetres, "20 miles");
+
+  new google.maps.Circle({
+    map: map,
+    center: officeLocation,
+    radius: thirtyMilesInMetres,
+    fillColor: "green",
+    strokeColor: "green",
+  });
+  addLabelToRadius(map, officeLocation, thirtyMilesInMetres, "30 miles");
 
   // plot employee addresses
-  locations.forEach((location) => {
-    new google.maps.Marker({
+  employeeData.forEach((data) => {
+    const marker = new google.maps.Marker({
       map: map,
-      position: location.latlng,
+      position: data.latlng,
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
         fillColor: "#000",
         fillOpacity: 1,
         scale: 2,
+        title: data.name,
       },
     });
+
+    addInfoWindowToMarker(
+      map,
+      marker,
+      `<span class="employee-label"><h4>${data.name}</h4><p>${data.postcode}</p></span>`
+    );
   });
+
+  updateMarkerCounters(map, officeLocation);
+}
+
+function customInfoWindow(content) {
+  return new google.maps.InfoWindow({
+    content: content,
+  });
+}
+
+function addInfoWindowToMarker(map, marker, content) {
+  const infoWindow = customInfoWindow(content);
+
+  marker.addListener("mouseover", () => {
+    infoWindow.open({
+      anchor: marker,
+      map,
+    });
+
+    infoWindow.setZIndex(1);
+  });
+
+  marker.addListener("mouseout", () => {
+    infoWindow.close();
+  });
+}
+
+function addLabelToRadius(map, circleCentre, radius, infoWindowContent) {
+  const spherical = google.maps.geometry.spherical;
+
+  const labelPosition = spherical.computeOffset(circleCentre, radius, 0);
+
+  const labelMarker = new google.maps.Marker({
+    map: map,
+    position: labelPosition,
+    icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      fillColor: "#fff",
+      fillOpacity: 0,
+      scale: 0,
+    },
+  });
+
+  customInfoWindow(infoWindowContent).open({
+    anchor: labelMarker,
+    map,
+  });
+}
+
+function updateMarkerCounters(map, officeLocation) {
+  const spherical = google.maps.geometry.spherical;
+
+  let counter20 = 0;
+  let counter30 = 0;
+
+  employeeData.forEach((data) => {
+    const distance = spherical.computeDistanceBetween(
+      officeLocation,
+      data.latlng
+    );
+
+    if (distance < twentyMilesInMetres) {
+      counter20 += 1;
+      counter30 += 1;
+    } else if (distance < thirtyMilesInMetres) {
+      counter30 += 1;
+    }
+  });
+
+  document.querySelector("#counter-20").innerHTML = counter20;
+  document.querySelector("#counter-30").innerHTML = counter30;
+  document.querySelector("#counter-total").innerHTML = employeeData.length;
 }
